10e668908ed5006beb4a31f369e9b3cd9cd70068
mirzaawais203 <mirzaawais203@gmail.com>
2024-03-16 15:34:50 +0500

    changes in header

--- src/Components/Home/Header/Header.jsx
+++ src/Components/Home/Header/Header.jsx
@@ -3,14 +3,27 @@ import "./Header.css";
 
 const header = () => {
   return (
-    <div className="header-top">
-      <div className="header">
-        <div className="header-content">
-          <h3>YOUR HOME IN THE PARK</h3>
-          <p>
-            Residential Apartments Located In The Heart of Bahria Town, Lahore.
-          </p>
+    <div>
+      <div className="header-top">
+        <div className="header">
+          <div className="header-content">
+            <h3>YOUR HOME IN THE PARK</h3>
+            <p>
+              Residential Apartments Located In The Heart of Bahria Town, Lahore.
+            </p>
+          </div>
         </div>
+      </div>
+      <div className="headermobileview">
+        <div className="headermobileview-img">
+          <img src="./HomePhotos/Header/header.jpg" alt="building" />
+        </div>
+        <div className="headermobileview-content">
+            <h3>YOUR HOME <br />IN THE PARK</h3>
+            <p>
+              Residential Apartments Located In The Heart of Bahria Town, Lahore.
+            </p>
+          </div>
       </div>
     </div>
   );
